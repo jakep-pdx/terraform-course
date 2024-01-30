6826e04fb29e80a79aadadd03cc7ae465108a4ac
jake.panchyshyn <jake.panchyshyn@standard.com>
2024-01-30 10:43:41 -0800

    04

---
+++ 04/outputs.tf
@@ -0,0 +1,10 @@
+# output "web_server_ip" {
+#   description = "Public IP Address of Web Server on EC2"
+#   value       = aws_instance.web_server.public_ip
+#   sensitive   = true
+# }
+
+output "vpc_id" {
+  description = "Output the ID for the primary VPC"
+  value = aws_vpc.vpc.id
+}
---
+++ 04/terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region                   = "us-west-2"
+  shared_credentials_files = ["../credentials"]
+}
